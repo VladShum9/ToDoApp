0c998f6433bfbda7349c3e6d9587cdbcd254d220
vlads <vlads@VLAD>
2025-03-29 13:53:05 +0100

    feat: add foreign key relation between ToDoTask and ApplicationUser

--- Persistence/Models/ToDoTask.cs
+++ Persistence/Models/ToDoTask.cs
@@ -1,8 +1,19 @@
-﻿namespace Persistence.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Persistence.Models
 {
     public class ToDoTask
     {
         public string Id { get; set; }
+
         public string Name { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; }
+
+        [ForeignKey(nameof(ApplicationUserId))]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
     }
 }
