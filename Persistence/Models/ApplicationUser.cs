debe93b07637577d09cb1932d34710cb626fdaae
vlads <vlads@VLAD>
2025-03-29 13:48:17 +0100

    feat: add ApplicationUser model

---
+++ Persistence/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Persistence.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string EmailConfirmed { get; set; }
+    }
+}
