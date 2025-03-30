83e416d4716281495089c5692e18d107a09217c2
vlads <vlads@VLAD>
2025-03-31 00:05:29 +0200

    refactor: add DbSet to Repository and implement it in created methods

--- Persistence/Repositories/Repository.cs
+++ Persistence/Repositories/Repository.cs
@@ -1,29 +1,27 @@
 ﻿using Persistence.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Persistence.Repositories
 {
-    public class Repository<T> : IRepository<T>
+    public class Repository<T> : IRepository<T> where T : class
     {
         private readonly ToDoAppDbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
         public Repository(ToDoAppDbContext dbContext) 
         {
             _dbContext = dbContext;
+            _dbSet = dbContext.Set<T>();
         }
 
         public async Task Add(T entity)
         {
-            await _dbContext.AddAsync(entity);
+            await _dbSet.AddAsync(entity);
         }
 
         public async Task Delete(string id)
         {
             T existing = await Get(id);
-            _dbContext.Remove(existing);
+            _dbSet.Remove(existing);
         }
 
         public async Task<T> Get(string id)
