da0a05c328d94f886a8f8edb4c906c6054b03813
vlads <vlads@VLAD>
2025-03-30 16:19:48 +0200

    feat: add ToDoAppDbContext

---
+++ Persistence/ToDoAppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Models;
+using System.Data.Entity;
+
+namespace Persistence
+{
+    public class ToDoAppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public Microsoft.EntityFrameworkCore.DbSet<ToDoTask> toDoTasks { get; set; }
+
+        public ToDoAppDbContext(DbContextOptions<ToDoAppDbContext> options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
