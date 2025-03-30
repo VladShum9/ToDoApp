2c89b28e6dccea7c8f19481ed5c03ad2ae698320
vlads <vlads@VLAD>
2025-03-30 19:55:14 +0200

    refactor: remove redundant properties from ApplicationUser

--- Persistence/Models/ApplicationUser.cs
+++ Persistence/Models/ApplicationUser.cs
@@ -4,14 +4,6 @@ namespace Persistence.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string Id { get; set; }
 
-        public string Name { get; set; }
-
-        public string Email { get; set; }
-
-        public string Password { get; set; }
-
-        public string EmailConfirmed { get; set; }
     }
 }
