1cd4a8589727c4ba2ed6226fd047cb2df3bc81fe
vlads <vlads@VLAD>
2025-03-31 15:03:04 +0200

    feat: add OperationResult class

---
+++ Persistence/Models/OperationResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Models
+{
+    class OperationResult<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+
+        public static OperationResult<T> SuccessResult(T data, string message)
+        {
+            return new OperationResult<T> { Success = true, Data = data, Message = message };
+        }
+
+        public static OperationResult<T> Failure(string message)
+        {
+            return new OperationResult<T> { Success = false, Message = message };
+        }
+    }
+}
