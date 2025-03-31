3e2c5ae143a3e59a401a4038307b9fed49133a1c
vlads <vlads@VLAD>
2025-03-31 14:01:01 +0200

    feat: add IToDoTaskService interface

---
+++ Backend/Interfaces/IToDoTaskService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Interfaces
+{
+    public interface IToDoTaskService
+    {
+        public Task Add(ToDoTask task);
+        public Task Delete(string id);
+        public Task Update(ToDoTask task);
+        public Task<List<ToDoTask>> GetAll();
+        public Task<ToDoTask> GetById(string id);
+    }
+}
