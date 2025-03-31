371629c6fd02e7eedfbd296f60737488c461f780
vlads <vlads@VLAD>
2025-03-31 14:05:18 +0200

    feat: add UnitOfWork

--- Backend/UnitOfWork.cs
+++ Backend/UnitOfWork.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Persistence;
+using Persistence.Interfaces;
+using Persistence.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,19 @@ namespace Backend
 {
     class UnitOfWork
     {
+        private readonly ToDoAppDbContext _dbContext;
+
+        private readonly IRepository<ToDoTask> _taskRepository;
+
+        UnitOfWork(ToDoAppDbContext dbContext, IRepository<ToDoTask> taskRepository)
+        {
+            _dbContext = dbContext;
+            _taskRepository = taskRepository;
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
     }
 }
