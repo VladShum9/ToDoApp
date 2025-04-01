ee77836588e4f081c9aea1a01df82b61f2308bc9
vlads <vlads@VLAD>
2025-04-01 17:51:39 +0200

    chore: register repositories, unit of work, and services in DI container

--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -1,3 +1,11 @@
+using Backend;
+using Backend.Interfaces;
+using Backend.Services;
+using Persistence.Interfaces;
+using Persistence.Models;
+using Persistence.Repositories;
+using System.Text.RegularExpressions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IRepository<ApplicationUser>, Repository<ApplicationUser>>();
+builder.Services.AddScoped<IRepository<ToDoTask>, Repository<ToDoTask>>();
+
+builder.Services.AddScoped<UnitOfWork>();
+
+builder.Services.AddScoped<IToDoTaskService, ToDoTaskService>();
 
 var app = builder.Build();
 
