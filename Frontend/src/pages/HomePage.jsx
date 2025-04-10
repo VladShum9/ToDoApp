5c7b6cad921e9004799559731b1ccad836ce2441
vlads <vlads@VLAD>
2025-04-10 16:10:54 +0200

    feat: Add getAllTasks function to fetch tasks from the API

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -2,5 +2,4 @@ import { useEffect, useState } from "react";
 
 export default function HomePage(){
     const [tasks, setTasks] = useState([]);
-
 }
--- Frontend/src/services/ToDoTaskService.js
+++ Frontend/src/services/ToDoTaskService.js
@@ -1,1 +1,14 @@
 import axios from "axios";
+
+const API_URL = "https://localhost:7029/api/toDoTask";
+
+export const getAllTasks = async () => {
+    try {
+        const response = await fetch('${API_URL}/GetAll');
+        
+        const data = await respose.json();
+        return data.data;
+    } catch (error) {
+        console.error("Error fetching tasks", error);
+    }
+}
