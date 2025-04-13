0dd6ac2a2fb3f3a10455a2d57d91ee5e50583c73
vlads <vlads@VLAD>
2025-04-13 17:45:55 +0200

    fix: fix GetAll function

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <header>
+        <h1>ToDo App</h1>
+      </header>
+        <Outlet />
+      <main>
+        
+      </main>
+      <footer>
+        <p>© 2025</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/services/ToDoTaskService.js
+++ Frontend/src/services/ToDoTaskService.js
@@ -4,9 +4,8 @@ const API_URL = "https://localhost:7029/api/toDoTask";
 
 export const getAllTasks = async () => {
     try {
-        const response = await fetch('${API_URL}/GetAll');
-        
-        const data = await respose.json();
+        const response = await fetch(`${API_URL}/GetAll`);
+        const data = await response.json();
         return data.data;
     } catch (error) {
         console.error("Error fetching tasks", error);
